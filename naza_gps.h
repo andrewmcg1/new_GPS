4e83e103d51a531852a36de316536c110dc9fca4
andrewmcg1 <greekgeek191@gmail.com>
2023-05-24 15:15:45 -0400

    gps update

--- naza_gps.h
+++ naza_gps.h
@@ -20,6 +20,15 @@ typedef uint8_t byte;
 
 #include <stdint.h>
 
+#include <coordinates.h>
+
+typedef union struct_to_int
+{
+    gps_data gps_structure;
+    gps_compass_data compass_structure;
+    uint8_t struct_as_int[GPS_PAYLOAD_SIZE];
+} struct_to_int;
+
 enum _ParseState
 {
     START_BYTE_1,
@@ -33,30 +42,60 @@ enum _ParseState
 };
 typedef _ParseState ParseState
 
-typedef struct __attribute__ ((packed)) gps_data_t
+typedef struct gps_data_t
 {
-    byte dateAndTime[4];
-    byte longitude[4];
-    byte latitude[4];
-    byte altitude[4];
-    byte horrizontalAccuracyEstimate[4];
-    byte verticalAccuracyEstimate[4];
-    byte unknown1[4];
-    byte NEDNorthVelocity[4];
-    byte NEDEashVelocity[4];
-    byte NEDDownVelocity[4];
-    byte positionDOP[2];
-    byte verticalDOP[2];
-    byte northingDOP[2];
-    byte eastingDOP[2];
+    long int dateAndTime;
+    long int longitude;
+    long int latitude;
+    long int altitude;
+    long int horrizontalAccuracyEstimate;
+    long int verticalAccuracyEstimate;
+    long int unknown1;
+    long int NEDNorthVelocity;
+    long int NEDEashVelocity;
+    long int NEDDownVelocity;
+    short int positionDOP;
+    short int verticalDOP;
+    short int northingDOP;
+    short int eastingDOP;
     byte numberOfSatalites;
     byte unknown2;
     byte fixType;
     byte unknown3;
     byte fixStatusFlag;
-    byte unknown4[2];
+    short int unknown4;
     byte xorMask;
-    byte sequenceNumber[2];
+    short int sequenceNumber;
+} gps_data_raw_t;
+
+typedef struct compass_data_raw_t
+{
+    short int x_vector;
+    short int y_vector;
+    short int z_vector;
+} compass_data_raw_t;
+
+typedef struct gps_data_t
+{
+    lla_t lla;
+    ned_waypoint_t ned;
+    double spd;        ///< speed in m/s
+    fixType_t fix;     ///< fix type
+    uint8_t sat;       ///< number of satellites
+    double headingNc;  ///< heading (not tilt compensated) in degrees
+    double cog;        ///< course over ground
+    double gpsVsi;     ///< vertical speed indicator (from GPS) in m/s (a.k.a. climb speed)
+    double hdop;       ///< horizontal dilution of precision
+    double vdop;       ///< vertical dilution of precision
+    uint8_t year;
+    uint8_t month;
+    uint8_t day;
+    uint8_t hour;
+    uint8_t minute;
+    uint8_t second;
+    uint64_t gps_data_received_ns;  ///< time on beaglebond board that gps is received
+    uint8_t gps_valid;
+
 } gps_data_t;
 
 extern gps_data_t gps_data;
