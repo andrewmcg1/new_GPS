f2a31eb20c849ecc5f14c5c5069957049f5b06e5
jshatto0225 <jshatto0225@gmail.com>
2023-05-24 13:34:00 -0400

    Changed

--- naza_gps.h
+++ naza_gps.h
@@ -22,12 +22,42 @@ typedef uint8_t byte;
 
 #include <coordinates.h>
 
+enum _ParseState
+{
+    START_BYTE_1,
+    START_BYTE_2,
+    MESSAGE_ID,
+    RECIEVE_PAYLOAD,
+    DECODE_PAYLOAD,
+    CHECKSUM_1,
+    CHECKSUM_2
+};
+typedef _ParseState ParseState
+
 typedef struct gps_data_t
 {
     byte dateAndTime[4];
     byte longitude[4];
     byte latitude[4];
     byte altitude[4];
+    byte horrizontalAccuracyEstimate[4];
+    byte verticalAccuracyEstimate[4];
+    byte unknown1[4];
+    byte NEDNorthVelocity[4];
+    byte NEDEashVelocity[4];
+    byte NEDDownVelocity[4];
+    byte positionDOP[2];
+    byte verticalDOP[2];
+    byte northingDOP[2];
+    byte eastingDOP[2];
+    byte numberOfSatalites;
+    byte unknown2;
+    byte fixType;
+    byte unknown3;
+    byte fixStatusFlag;
+    byte unknown4[2];
+    byte xorMask;
+    byte sequenceNumber[2];
 } gps_data_t;
 
 extern gps_data_t gps_data;
